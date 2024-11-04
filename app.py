734df5d57e51c626cf68659d2949146547f1c493
imanavyblue <134798126+imanavyblue@users.noreply.github.com>
2024-11-04 19:09:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, jsonify
 import cv2
 import math
 import cvzone
 from ultralytics import YOLO
 
 # กำหนดเส้นทางของวิดีโอและโมเดล YOLO
-video_path = "media/00043.mp4"  # วิดีโอตัวอย่าง (หรือใช้กล้อง)
+video_path = "media/C0008.mp4"  # วิดีโอตัวอย่าง (หรือใช้กล้อง)
 cap = cv2.VideoCapture(video_path)
 model = YOLO("wieght/best.pt")
 
@@ -14,12 +14,16 @@ classNames = ['With Helmet', 'Without Helmet']
 
 app = Flask(__name__)
 
+detected_classes = []
+
 def generate_frames():
+    global detected_classes
     while True:
         success, img = cap.read()
         if not success:
             break
         results = model(img, stream=True)
+        detected_classes = []  # รีเซ็ตข้อมูลการตรวจจับทุกครั้งที่อ่านเฟรมใหม่
         for r in results:
             boxes = r.boxes
             for box in boxes:
@@ -31,7 +35,11 @@ def generate_frames():
                 conf = math.ceil((box.conf[0] * 100)) / 100
                 cls = int(box.cls[0])
 
-                cvzone.putTextRect(img, f'{classNames[cls]} {conf}', (max(0, x1), max(35, y1)), scale=1, thickness=1)
+                # เก็บข้อมูลคลาสที่ตรวจจับได้
+                detected_classes.append(f'{classNames[cls]}: {conf:.2f}')
+
+                # วาดกรอบและแสดงข้อมูลบนเฟรม
+                cvzone.putTextRect(img, f'{classNames[cls]} {conf}', (max(0, x1), max(35, y1)), scale=2, thickness=3)
 
         # แปลงภาพเป็น JPEG
         ret, buffer = cv2.imencode('.jpg', img)
@@ -46,6 +54,10 @@ def index():
 @app.route('/video_feed')
 def video_feed():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/get_detected_classes')
+def get_detected_classes():
+    return jsonify(detected_classes)
 
 if __name__ == "__main__":
     app.run(debug=True)
